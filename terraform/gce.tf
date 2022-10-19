8d5f2de27c6f5c07d045ded2d4421958d0da766c
sylvio.pedroza <sylvio.pedroza@gmail.com>
2022-10-19 17:01:36 -0300

    minor

--- terraform/gce.tf
+++ terraform/gce.tf
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 module "instance_template" {
   source  = "terraform-google-modules/vm/google//modules/instance_template"
   version = "~> 7.6"
@@ -7,7 +23,7 @@ module "instance_template" {
   region               = var.region
   network              = module.vpc.network_name
   subnetwork           = "subnet-${var.region}"
-  service_account      = local.service_account
+  service_account      = google_service_account.service_account.id
   labels               = local.resource_labels
   source_image         = "cos-stable-97-16919-29-40"
   source_image_project = "cos-cloud"
--- terraform/network.tf
+++ terraform/network.tf
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 module "vpc" {
   source       = "terraform-google-modules/network/google"
   version      = "~> 5.0"
@@ -31,4 +47,27 @@ resource "google_compute_firewall" "allow_ssh" {
   target_tags = [
     "allow-ssh"
   ]
+}
+
+resource "google_compute_router" "nat_router" {
+  name    = "${module.vpc.network_name}-nat-router"
+  network = module.vpc.network_self_link
+  region  = var.region
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "nat_gateway" {
+  name                               = "${module.vpc.network_name}-nat-gw"
+  router                             = google_compute_router.nat_router.name
+  region                             = google_compute_router.nat_router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
 }
