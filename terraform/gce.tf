5e08a230f4edcc8203f1dd2d8e1b3393532e52d8
sylvio <sylvio.pedroza@gmail.com>
2022-12-08 20:57:11 +0000

    minor

--- terraform/gce.tf
+++ terraform/gce.tf
@@ -14,12 +14,12 @@
  * limitations under the License.
  */
 
-module "instance_template_be" {
+module "instance_template" {
   source  = "terraform-google-modules/vm/google//modules/instance_template"
   version = "~> 7.6"
 
   project_id           = var.project_id
-  name_prefix          = "${var.application_name}-be"
+  name_prefix          = "${var.application_name}"
   region               = var.region
   network              = module.vpc.network_name
   subnetwork           = "subnet-${var.region}"
@@ -45,15 +45,15 @@ module "instance_template_be" {
   ]
 }
 
-module "mig_be" {
+module "mig" {
   source  = "terraform-google-modules/vm/google//modules/mig"
   version = "~> 7.6.0"
 
   project_id        = var.project_id
   region            = var.region
   target_size       = 2
-  # hostname          = var.application_name
-  instance_template = module.instance_template_be.self_link
+  hostname          = var.application_name
+  instance_template = module.instance_template.self_link
 
   named_ports = [
     {
